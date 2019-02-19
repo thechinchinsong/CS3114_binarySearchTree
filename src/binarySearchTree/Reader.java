e4673fc1cd1797f58a2486f1c1672cdaa52d376e
thechinchinsong <38168903+thechinchinsong@users.noreply.github.com>
2019-02-19 18:06:10 -0500

    Added the driver class and finished up the reader class. Minor commenting and formatting.

--- src/binarySearchTree/Reader.java
+++ src/binarySearchTree/Reader.java
@@ -7,85 +7,85 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class Reader {
+    Scanner scan;
 
-    public static void main(String[] args) throws FileNotFoundException {
 
-        if (args.length == 1) {
-            File file = new File(args[0]);
-            Scanner scan = new Scanner(file);
+    public Reader(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        scan = new Scanner(file);
+    }
 
-            while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                String[] parts = line.split("\\s+");
-                if (parts[0].contentEquals("insert") && parts.length == 6) {
 
-                    int x, y, w, h;
-                    try {
-                        x = Integer.parseInt(parts[2]);
-                        y = Integer.parseInt(parts[3]);
-                        w = Integer.parseInt(parts[4]);
-                        h = Integer.parseInt(parts[5]);
-                    }
-                    catch (NumberFormatException e) {
-                        System.out.print("Invalid Command Format");
-                    }
-                    // insert(parts[1], x,y,w,h);
+    public void reader(RectangleBST tree) {
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] parts = line.split("\\s+");
+            if (parts[0].contentEquals("insert") && parts.length == 6) {
 
+                int x = 0, y = 0, w = 0, h = 0;
+                try {
+                    x = Integer.parseInt(parts[2]);
+                    y = Integer.parseInt(parts[3]);
+                    w = Integer.parseInt(parts[4]);
+                    h = Integer.parseInt(parts[5]);
                 }
-                else if (parts[0].contentEquals("remove")
-                    && parts.length == 2) {
-                    // remove(parts[1]);
-
+                catch (NumberFormatException e) {
+                    System.out.print("Invalid Command Format");
                 }
-                else if (parts[0].contentEquals("remove")
-                    && parts.length == 5) {
-
-                    int x, y, w, h;
-                    try {
-                        x = Integer.parseInt(parts[2]);
-                        y = Integer.parseInt(parts[3]);
-                        w = Integer.parseInt(parts[4]);
-                        h = Integer.parseInt(parts[5]);
-                    }
-                    catch (NumberFormatException e) {
-                        System.out.print("Invalid Command Format");
-                    }
-                    // remove(x,y,w,h);
-
-                }
-                else if (parts[0].contentEquals("search")
-                    && parts.length == 2) {
-                    // search(parts[1]);
-
-                }
-                else if (parts[0].contentEquals("dump") && parts.length == 1) {
-                    // dump();
-
-                }
-                else if (parts[0].contentEquals("regionsearch")
-                    && parts.length == 5) {
-
-                    int x, y, w, h;
-                    try {
-                        x = Integer.parseInt(parts[2]);
-                        y = Integer.parseInt(parts[3]);
-                        w = Integer.parseInt(parts[4]);
-                        h = Integer.parseInt(parts[5]);
-                    }
-                    catch (NumberFormatException e) {
-                        System.out.print("Invalid Command Format");
-                    }
-                    // regionsearch(x,y,w,h);
-
-                }
-                else if (parts[0].contentEquals("intersections")
-                    && parts.length == 1) {
-                    // intersections();
-
-                }
+                tree.insert(parts[1], x, y, w, h);
 
             }
-            scan.close();
+            else if (parts[0].contentEquals("remove") && parts.length == 2) {
+                tree.remove(parts[1]);
+
+            }
+            else if (parts[0].contentEquals("remove") && parts.length == 5) {
+
+                int x = 0, y = 0, w = 0, h = 0;
+                try {
+                    x = Integer.parseInt(parts[2]);
+                    y = Integer.parseInt(parts[3]);
+                    w = Integer.parseInt(parts[4]);
+                    h = Integer.parseInt(parts[5]);
+                }
+                catch (NumberFormatException e) {
+                    System.out.print("Invalid Command Format");
+                }
+                tree.remove(x, y, w, h);
+
+            }
+            else if (parts[0].contentEquals("search") && parts.length == 2) {
+                tree.search(parts[1]);
+
+            }
+            else if (parts[0].contentEquals("dump") && parts.length == 1) {
+                tree.dump();
+
+            }
+            else if (parts[0].contentEquals("regionsearch")
+                && parts.length == 5) {
+
+                int x = 0, y = 0, w = 0, h = 0;
+                try {
+                    x = Integer.parseInt(parts[2]);
+                    y = Integer.parseInt(parts[3]);
+                    w = Integer.parseInt(parts[4]);
+                    h = Integer.parseInt(parts[5]);
+                }
+                catch (NumberFormatException e) {
+                    System.out.print("Invalid Command Format");
+                }
+                tree.regionSearch(x, y, w, h);
+
+            }
+            else if (parts[0].contentEquals("intersections")
+                && parts.length == 1) {
+                tree.intersections();
+
+            }
+
         }
+        scan.close();
+
     }
 }
---
+++ src/binarySearchTree/Rectangle1.java
@@ -0,0 +1,40 @@
+package binarySearchTree;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Driver that is used to create the parser and the tree, then using
+ * the reader method in the parser to output information based on the
+ * input and tree.
+ *
+ * @author Ben Zevin
+ * @author Yaoquan Song
+ * @version 2019.2.19
+ */
+public class Rectangle1 {
+
+    /**
+     * Default constructor
+     */
+    public Rectangle1() {
+
+    }
+
+
+    /**
+     * Main function for parsing and output.
+     * 
+     * @param args
+     *            input for the parser
+     * @throws FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        Reader parser;
+        RectangleBST tree = new RectangleBST();
+        if (args.length == 1) {
+            parser = new Reader(args[0]);
+            parser.reader(tree);
+        }
+    }
+
+}
